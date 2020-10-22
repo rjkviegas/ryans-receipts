fbec5ebc9f3d12cda04a7945393845bf48881051
Ryan Viegas <rjkviegas@gmail.com>
2020-10-22 18:31:48 +0100

    refactor out excess parameters and inline header

--- src/receipt.js
+++ src/receipt.js
@@ -1,46 +1,37 @@
 function receipt (menu, order, taxRate=8.64) {
-    let result = '';
-    result += header(menu, order);
+    let result = `${menu.shopName}\n${menu.address}\n${phoneNumFormat(menu.phone)}\n`;
+    result += `${order.customer}\n`;
     for (let item of order.items) {
         result += `${item.id}\t${item.quantity} x` +
             ` ${usd(menu.prices[0][item.id])} =` +
             ` ${usd(item.quantity * menu.prices[0][item.id])}\n`;
     }
-    result += `Tax\t${usd(taxTotal(preTaxTotal(menu, order), taxRate))}\n` +
-        `Total Amount:\t${usd(totalAmount(menu, order))}`;
+    result += `Tax\t${usd(taxTotal(preTaxTotal()))}\n` +
+        `Total Amount:\t${usd(totalAmount())}`;
     return result;
-
-    function header (aMenu, anOrder) {
-        let result = '';
-        result += `${aMenu.shopName}\n`;
-        result += `${aMenu.address}\n`;
-        result += phoneNumFormat(aMenu.phone);
-        result += `${anOrder.customer}\n`;
-        return result;
-    }
 
     function phoneNumFormat (aPhoneNum) {
         return `+${aPhoneNum[0]} ` +
         `(${aPhoneNum.slice(1, 4)}) ` +
         `${aPhoneNum.slice(4, 7)}` +
-        `-${aPhoneNum.slice(7)}\n`
+        `-${aPhoneNum.slice(7)}`
     }
 
-    function totalAmount (aMenu, anOrder) {
-        return preTaxTotal(aMenu, anOrder) + taxTotal(preTaxTotal(aMenu, anOrder), taxRate);
+    function totalAmount () {
+        return preTaxTotal() + taxTotal(preTaxTotal());
     }
 
-    function preTaxTotal (aMenu, anOrder) {
+    function preTaxTotal () {
         let result = 0;
-        for (let item of anOrder.items) {
-            let itemPrice = aMenu.prices[0][item.id];
+        for (let item of order.items) {
+            let itemPrice = menu.prices[0][item.id];
             result += (item.quantity*itemPrice);
         }
         return result;
     }
 
-    function taxTotal (anAmount, aTaxRate) {
-        return anAmount*aTaxRate/100;
+    function taxTotal (anAmount) {
+        return anAmount*taxRate/100;
     }
 
     function usd (anAmount) {
