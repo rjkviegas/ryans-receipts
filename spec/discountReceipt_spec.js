c38a109a60c98085d8195bd7a8f53456eb04484c
Ryan Viegas <rjkviegas@gmail.com>
2020-10-26 13:20:11 +0000

    add pad variable to test

--- spec/discountReceipt_spec.js
+++ spec/discountReceipt_spec.js
@@ -5,25 +5,27 @@ const totalDiscOrderJson = require('./sampleOrders/discounted/totalDiscOrder.jso
 
 describe('receipt with muffin discount', function () {
     const testReceipt = receipt(menuJson, muffinOrderJson, 10.00);
+    const pad = 25;
     it('returns percentage and amount applied to', function () {
         expect(testReceipt).toContain(
-            'Disc:'.padEnd(25) + '10% from $4.05\n'
+            'Disc:'.padEnd(pad) + '10% from $4.05\n'
         );
     });
     it('returns tax total', function () {
         expect(testReceipt).toContain(
-            'Tax:'.padEnd(25) + '$0.31\n'
+            'Tax:'.padEnd(pad) + '$0.31\n'
         );
     });
     it('returns total amount', function () {
         expect(testReceipt).toContain(
-            'Total Amount:'.padEnd(25) + '$3.96\n'
+            'Total Amount:'.padEnd(pad) + '$3.96\n'
         );
     });
 });
 
 describe('receipt with total over 50 discount', function () {
     const testReceipt = receipt(menuJson, totalDiscOrderJson, 80.00);
+    const pad = 25;
     it('returns percentage and amount applied to', function () {
         expect(testReceipt).toContain(
             '5% discount applies as total is over $50.00\n'
@@ -31,7 +33,7 @@ describe('receipt with total over 50 discount', function () {
     });
     it('returns total amount', function () {
         expect(testReceipt).toContain(
-            'Final Amount:'.padEnd(25) + '$75.34\n'
+            'Final Amount:'.padEnd(pad) + '$75.34\n'
         );
     });
 });
