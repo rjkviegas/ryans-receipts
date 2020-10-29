2d1937022c9f53bb2e33df2aa2499ab1d60d5a47
Ryan Viegas <rjkviegas@gmail.com>
2020-10-29 10:53:50 +0000

    Create default layout and views.

--- site/ryansreceipts.js
+++ site/ryansreceipts.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 
 const app = express();
+
+//configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
