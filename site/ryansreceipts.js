6f109816fae3fa381c89c2b08b051bc2c2171541
Ryan Viegas <rjkviegas@gmail.com>
2020-10-29 10:44:48 +0000

    Add minimal Express server

---
+++ site/ryansreceipts.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.type('text/plain');
+    res.send("Ryan's Receipts");
+});
+
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send("About Ryan's Receipts")
+});
+
+// custom 404 page
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    'press Ctrl-C to terminate.'
+));
