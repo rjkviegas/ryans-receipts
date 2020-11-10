71faea64efb42a1b5e5f3f859702d5e6fbd24fb3
Ryan Viegas <rjkviegas@gmail.com>
2020-11-10 22:23:32 +0000

    Clean ryansreceipts_soec.js

--- spec/ryansreceipts_spec.js
+++ spec/ryansreceipts_spec.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 const menuAndOrderJSON = require('../src/json/sampleOrders/discounted/menuAndOrder.json');
-const { htmlReceipt } = require('../src/lib/receipt');
+const createReceipt = require('../src/lib/createReceiptData');
 
 
 describe('Server',function () {
@@ -26,69 +26,12 @@ describe('Server',function () {
             });
         });
         it("Status 201", function() {
-            
             expect(data.status).toBe(201);
         });
         it("Body", function() {
-            expect(data.body).toEqual({
-                "receipt": {
-                    "shopName":"The Coffee Connection",
-                    "address":"123 Lakeside Way",
-                    "phone":"16503600708",
-                    "customer":"Geraldine",
-                    "taxRate":8.64,
-                    "items":[
-                        {
-                            "id":"Blueberry Muffin",
-                            "quantity":10,
-                            "unitPrice":4.05,
-                            "amount":40.5,
-                            "discPercent":10,
-                            "discAmount":4.05,
-                            "totalAmount":36.45
-                        },
-                        {
-                            "id":"Chocolate Chip Muffin"
-                            ,"quantity":8,
-                            "unitPrice":4.05,
-                            "amount":32.4,
-                            "discPercent":10,
-                            "discAmount":3.24,
-                            "totalAmount":29.159999999999997
-                        },
-                        {
-                            "id":"Tea",
-                            "quantity":3,
-                            "unitPrice":3.65,
-                            "amount":10.95,
-                            "discPercent":0,
-                            "discAmount":0,
-                            "totalAmount":10.95
-                        }
-                    ],
-                    "itemDiscounts":[
-                        {
-                            "items": [
-                                "Blueberry Muffin",
-                                "Chocolate Chip Muffin"
-                            ],
-                            "percent":10,
-                            "preAmount":72.9
-                        }
-                    ],
-                    "preTaxTotal":76.56,
-                    "taxTotal":6.614784000000001,
-                    "totalAmount":83.174784,
-                    "totalDiscount": {
-                        "limit":50,
-                        "percent":5,
-                        "amount":83.174784
-                    },
-                    "finalAmount":79.0160448,
-                    "cash":100,
-                    "change":20.983955199999997
-                }
-            });
+            expect(data.body).toEqual(
+                { "receipt": createReceipt(menuAndOrderJSON.menu, menuAndOrderJSON.order) }
+            );
         });
     });
     describe("POST /makehtmlreceipt", function() {
@@ -101,17 +44,12 @@ describe('Server',function () {
                 json: true
             }, function(error, response, body) {
                 data.status = response.statusCode;
-                // data.body = body;
                 done();
             });
         });
         it("Status 201", function() {
-            
             expect(data.status).toBe(201);
         });
-        // it("Body", function() {
-        //     expect(data.body).toEqual();
-        // });
     });
     
 });
