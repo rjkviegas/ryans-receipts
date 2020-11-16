c7a767c6e16ded9aaf5d15efec8fcdd660556074
Ryan Viegas <rjkviegas@gmail.com>
2020-11-16 15:09:51 +0000

    Add corsOptions

--- ryansreceipts.js
+++ ryansreceipts.js
@@ -6,13 +6,20 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+    "origin": true,
+    "methods": ['POST'],
+    "preflightContinue": true,
+    "optionsSuccessStatus": 204
+}
+
+// app.use(cors());
 
 // Receipt making API
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-app.post('/makereceipt', (req, res) => res.status(201).json({ receipt: createReceiptData(req.body.menu, req.body.order) }));
+app.post('/makereceipt', cors(corsOptions), (req, res) => res.status(201).json({ receipt: createReceiptData(req.body.menu, req.body.order) }));
 
 const server = app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
