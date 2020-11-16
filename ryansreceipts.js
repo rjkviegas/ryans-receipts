a54a5218dd6501ffd4e369f0b7ba429857b845f0
Ryan Viegas <rjkviegas@gmail.com>
2020-11-16 14:50:19 +0000

    Delegate cors

--- ryansreceipts.js
+++ ryansreceipts.js
@@ -6,13 +6,24 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
+
+var allowlist = ['http://192.168.89.83:3000', 'http://example2.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 // Receipt making API
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false}));
 
-app.post('/makereceipt', (req, res) => res.status(201).json({ receipt: createReceiptData(req.body.menu, req.body.order) }));
+app.post('/makereceipt', cors(corsOptionsDelegate), (req, res) => res.status(201).json({ receipt: createReceiptData(req.body.menu, req.body.order) }));
 
 const server = app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
