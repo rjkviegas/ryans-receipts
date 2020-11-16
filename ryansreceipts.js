8256e34a8bda886dc55e47baf3d283627a475ae5
Ryan Viegas <rjkviegas@gmail.com>
2020-11-16 12:13:58 +0000

    Add corsOptions for POST route

--- ryansreceipts.js
+++ ryansreceipts.js
@@ -6,14 +6,20 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+    origin: true,
+    methods: 'POST'
+    // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
 // Simple Usage (Enable All CORS Requests)
-app.use(cors());
+// app.use(cors());
 
 // Receipt making API
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-app.post('/makereceipt', (req, res) => res.status(201).json({ receipt: createReceiptData(req.body.menu, req.body.order) }));
+app.post('/makereceipt', cors(corsOptions), (req, res) => res.status(201).json({ receipt: createReceiptData(req.body.menu, req.body.order) }));
 
 const server = app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
