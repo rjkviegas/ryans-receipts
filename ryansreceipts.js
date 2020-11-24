d02ab65a1cff0e2273098b4a1814e42296faf209
Ryan Viegas <rjkviegas@gmail.com>
2020-11-24 17:39:59 +0000

    Add redirect handler

--- ryansreceipts.js
+++ ryansreceipts.js
@@ -11,7 +11,7 @@ app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.redirect('https://ryansreceipts.com'));
+app.get('/', handlers.redirectHome);
 
 app.post('/makereceipt', handlers.makeReceipt);
 
--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -1,6 +1,8 @@
 const createReceiptData = require('./createReceiptData');
 const { receipt, htmlReceipt } = require('../lib/receipt');
 
+exports.redirectHome = (req, res) => res.redirect('https://ryansreceipts.com');
+
 exports.makeReceipt = (req, res) => res.json({ receipt: createReceiptData(req.body.menu, req.body.order) })
 
 exports.plainTextReceipt = (req, res) => res.send(receipt(req.body.menu, req.body.order));
